3f53babb5dbe221c53466d3ab70e550328a9f7c7
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-03-24 23:55:27 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CoolTreasure;
+/*
+ºprivate fields
+ºpublic methods
+ºEncapsulation
+ºBasic validation
+ */					
+public class Program
+{
+	public static void Main()
+	{
+		TreasureChest CoolChest = new TreasureChest();
+		Console.WriteLine("Ahoy matey, take a look in my special " +
+		"treasure chest \nwhile i make a deposit");
+		CoolChest.AddGold();
+		Console.WriteLine($"Gold added! {CoolChest.DisplayGold()}");
+		CoolChest.AddGold();
+		Console.WriteLine($"Gold added! {CoolChest.DisplayGold()}");
+		CoolChest.RemoveGold();
+		Console.WriteLine($"Gold removed! {CoolChest.DisplayGold()}");
+		CoolChest.RemoveGold();
+		Console.WriteLine($"Gold removed! {CoolChest.DisplayGold()}");
+		
+	}
+}
---
+++ Treasure.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace CoolTreasure;
+class TreasureChest
+{ 
+	private int _treasureAmount;
+
+	
+	private int TreasureAmount
+	{
+		get {return _treasureAmount;}
+		set {_treasureAmount = value;}
+	}
+	public int AddGold()
+	{
+		TreasureAmount = TreasureAmount +1000;
+		return TreasureAmount;
+	}
+
+	public string RemoveGold()
+	{
+		if (TreasureAmount < 1000)
+		{
+			Console.WriteLine("Cannot remove more Gold than is here in the chest");
+			return "0";
+		}
+		else
+		{
+			TreasureAmount = TreasureAmount - 1000;
+			if (TreasureAmount == 0)
+			{
+				TreasureAmount++;
+				Console.WriteLine("Agh pesky BUG get out of here! Darn I dropped a coin in there, I'll just leave it");
+			}
+		}	
+		
+		return TreasureAmount.ToString();
+	}
+
+	public string DisplayGold()
+	{
+		return $"Current gold in chest: {TreasureAmount}";
+	}
+}
+
+
+
+/*
+ public string Model;
+        public int Speed;
+
+        public Car(string model)
+        {
+            this.Model = model;
+            this.Speed = 0;
+ */
